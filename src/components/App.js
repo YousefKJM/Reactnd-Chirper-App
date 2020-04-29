d3ea4f073a29cded76f4e50b540688a1c9cc57db
Yousef Majeed <uaso0of056@gmail.com>
2020-04-29 16:55:22 +0300

    finish project

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Nav from './Nav'
+import Dashboard from './Dashboard'
+import TweetPage from './TweetPage'
+import NewTweet from './NewTweet'
+import LoadingBar from 'react-redux-loading'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+  componentDidMount () {
+    const { dispatch, loading } = this.props
+    if (loading === true) {
+      dispatch(handleInitialData())
+    }
+  }
   render() {
     return (
-      <div>
-        Starter Code
-      </div>
-    )
+      <Router basename='/projects/redux-twitter'>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/tweet/:id' component={TweetPage} />
+                  <Route path='/new' component={NewTweet} />
+                </div>}
+          </div>
+        </Fragment>
+      </Router>
+    );
   }
 }
 
-export default App
+function mapStateToProps ({ authedUser, users }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
